677370c3dde64f2bcad3814e2f8a8c6c08109300
Carly Clift <carlymclift@gmail.com>
2020-08-18 15:16:14 -0600

    Style out basic heading on the app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-header-text">Rancid Tomatillos</h1>
-        <button className="App-login-button">Login</button>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // constructor() {
+  //   super()
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-header-text">Rancid Tomatillos</h1>
+          <button className="App-login-button">Login</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
