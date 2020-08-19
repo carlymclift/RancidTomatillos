ed82724695bc8e8db8a632e3581b5b681967fcca
Jake West <jacobkwest93@gmail.com>
2020-08-19 14:56:28 -0600

    Change Login to be a class based component; add validate login/submit login methods #2

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,25 +1,50 @@
-import React from 'react'
-import './Login.css'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Login.css';
 
-const Login = () => {
-  //Eventually, I think we'll need to turn this into a class component that can handle the POST. -jkw
-  return (
-    <div className="Login-container">
-      <form>
-        <input
-          type='text'
-          placeholder='Username'
-          name='username'
-        />
-        <input
-          type='password'
-          placeholder='Password'
-          name='password'
-        />
-        <button>Submit</button>
-      </form>
-    </div>
-  )
+class Login extends Component {
+  submitLogin = (event) => {
+    event.preventDefault()
+    if(this.validateLogin()) {
+      return (console.log('YAY'))
+    };
+  }
+
+  validateLogin = () => {
+    const loginInput = document.getElementById('username-input').value;
+    const passwordInput = document.getElementById('password-input').value;
+    console.log({loginInput, passwordInput})
+
+    if(loginInput === 'greg@turing.io' && passwordInput === 'abc123') {
+        return true
+    }
+  }
+
+  render() {
+    return (
+      <div className="Login-container">
+        <form>
+          <input
+            type='text'
+            placeholder='Username'
+            name='username'
+            id='username-input'
+          />
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            id='password-input'
+          />
+          <button onClick={this.submitLogin}>Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+Login.propTypes = {
+  submitLogin: PropTypes.func
 }
 
 export default Login
