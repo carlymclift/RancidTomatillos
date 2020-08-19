c4e9e3ad2442537d759bf32ff94d13760a5f44cb
Jake West <jacobkwest93@gmail.com>
2020-08-19 08:47:32 -0600

    Add form for login & styling #2

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -2,9 +2,22 @@ import React from 'react'
 import './Login.css'
 
 const Login = () => {
+  //Eventually, I think we'll need to turn this into a class component that can handle the POST. -jkw
   return (
     <div className="Login-container">
-      Login!!
+      <form>
+        <input
+          type='text'
+          placeholder='Username'
+          name='username'
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          name='password'
+        />
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
