de1bc2f10cc0613c50463e9b587177305ec09c82
Jake West <jacobkwest93@gmail.com>
2020-08-21 08:36:14 -0600

    Refactor MoviePage to have state & new componentDidMount method #4

--- src/MovieDetails/MoviePage.js
+++ src/MovieDetails/MoviePage.js
@@ -1,39 +1,65 @@
 import React, { Component } from "react";
-// import './MoviePage.scss';
-// import { getSingleMovieDetails } from "./NetworkRequests/APIRequests";
+import './MoviePage.css';
+import { getSingleMovieDetails } from "../NetworkRequests/APIRequests";
 
 class MoviePage extends Component {
-  constructor() {
+  constructor( {foundMovieId} ) {
     super();
-    this.state = {};
+    this.state = {
+      average_rating: 0,
+      backdrop_path: '',
+      budget: 0,
+      genres: [],
+      id: 0,
+      overview: '',
+      poster_path: '',
+      release_date: '',
+      revenue: 0,
+      runtime: 0,
+      tagline: '',
+      title: '',
+    };
   }
 
-  // componentDidMount() {
-  //   this.getMovieDetails();
-  // }
 
-  // getMovieDetails = async () => {
-  //   try {
-  //     const data = await getSingleMovieDetails(this.id)
-  //     this.setState({
-  //       title: data.movie.title,
-  //       releaseDate: data.movie.release_date,
-  //       backDrop: data.movie.backdrop_path,
-  //       overview: data.movie.overview,
-  //       genres: data.movie.genres,
-  //       budget: data.movie.budget,
-  //       revenue: data.movie.revenue,
-  //       runtime: data.movie.runtime,
-  //       tagLine: data.movie.tagline,
-  //       averageRating: data.movie.average_rating
-  //     });
-  //   } catch (error) {
-  //     this.setState({error});
-  //   }
-  // }
+  componentDidMount = () => {
+    fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.foundMovieId}`)
+      .then(response => response.json())
+      .then(data => data.movie)
+      .then(movieData => {
+        this.setState({
+        average_rating: movieData.average_rating,
+        backdrop_path: movieData.backdrop_path,
+        budget: movieData.budget,
+        genres: movieData.genres,
+        id: movieData.id,
+        overview: movieData.overview,
+        poster_path: movieData.poster_path,
+        release_date: movieData.release_date,
+        revenue: movieData.revenue,
+        runtime: movieData.runtime,
+        tagline: movieData.tagline,
+        title: movieData.title,
+        })
+      })
+  }
 
   render() {
-    return "hi"
+    return (
+      <div className="Movie-Page" style={{ backgroundImage: `url(${this.state.backdrop_path})`}}>
+        <h2>{this.state.title}</h2>
+        <p>{this.tagline}</p>
+        <img src={this.state.poster_path} />
+        <p>{this.state.overview}</p>
+        <div className="movie-details">
+          <p>{this.state.release_date}</p>
+          <p>{this.state.budget}</p>
+          <p>{this.state.revenue}</p>
+          <p>{this.state.runtime}</p>
+        </div>
+
+      </div>
+    )
   }
 }
 
