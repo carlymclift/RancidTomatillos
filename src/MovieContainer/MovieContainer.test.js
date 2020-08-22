3a5a3511cb160a958ecfe41886e5971ad3466686
Jake West <jacobkwest93@gmail.com>
2020-08-22 13:26:01 -0600

    Add MovieContainer test code #16

--- src/MovieContainer/MovieContainer.test.js
+++ src/MovieContainer/MovieContainer.test.js
@@ -4,5 +4,39 @@ import { screen, fireEvent, render } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
 describe('MovieContainer', () => {
-  it('', () => {})
+  it('should render a number of movies equal to the length of the array passed in', () => {
+    const movies = [
+      {
+        id: 111,
+        title: 'Movie 1',
+        poster_path: 'www.movie1_poster.com',
+        backdrop_path: 'www.movie1_backdrop.com',
+        average_rating: 1,
+        release_date: '2020-01-01'
+      },
+      {
+        id: 2,
+        title: 'Movie 2',
+        poster_path: 'www.movie2_poster.com',
+        backdrop_path: 'www.movie2_backdrop.com',
+        average_rating: 2,
+        release_date: '2020-02-02'
+      }
+    ]
+    const mockShowMovieDetails = jest.fn()
+
+    render(<MovieContainer
+      movies={movies}
+      showMovieDetails={mockShowMovieDetails}
+      />
+    )
+
+    const headingOne = screen.getByRole('heading', {name: 'Movie 1'})
+    const headingTwo = screen.getByRole('heading', {name: 'Movie 2'})
+
+    screen.debug()
+
+    expect(headingOne).toBeInTheDocument();
+    expect(headingTwo).toBeInTheDocument();
+  })
 })
