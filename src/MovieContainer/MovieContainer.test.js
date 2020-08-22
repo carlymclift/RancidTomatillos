4febb88ac47f885d35b4e4c765b56ebf83601016
Jake West <jacobkwest93@gmail.com>
2020-08-22 13:12:19 -0600

    Add MovieContainer test file & boilerplate #16

---
+++ src/MovieContainer/MovieContainer.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import MovieContainer from './MovieContainer'
+import { screen, fireEvent, render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+describe('MovieContainer', () => {
+  it('', () => {})
+})
