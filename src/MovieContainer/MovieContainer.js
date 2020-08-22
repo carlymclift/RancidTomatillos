ed5ad5c2ae9c2658ee14c2bf7eb0c9aacc15225c
Jake West <jacobkwest93@gmail.com>
2020-08-22 13:25:23 -0600

    Fix import statement to reflect new file structure

--- src/MovieContainer/MovieContainer.js
+++ src/MovieContainer/MovieContainer.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './MovieContainer.css'
-import MovieCard from './MovieCard'
+import MovieCard from '../MovieCard/MovieCard'
 
 const MovieContainer = ( {movies, showMovieDetails} ) => {
     const movieCards = movies.map(movie => {
         return (<MovieCard {...movie} key={movie.id} showMovieDetails={showMovieDetails}/>)
     })
-      
+
     return (
         <div className="Movie-container">
             {movieCards}
