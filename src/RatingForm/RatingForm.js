151ec0a44820c7597bd73bc72d90991622923670
Jake West <jacobkwest93@gmail.com>
2020-08-24 17:53:10 -0600

    Refactored handleSubmit to pass correct data from props; POST functionality working! #17

--- src/RatingForm/RatingForm.js
+++ src/RatingForm/RatingForm.js
@@ -13,20 +13,22 @@ class RatingForm extends Component {
 
   updateForm(event) {
     const rating = event.target.value;
-    this.setState({ formInput: rating })
+    this.setState({ formInput: parseInt(rating) })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    addMovieRating(this.props.props.userId, this.props.props.foundMovieId, this.state.formInput)
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h4>Add Review</h4>
         <select value={this.state.formInput} onChange={this.updateForm}>
           {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(number => <option value={number}>{number}</option>)}
         </select>
-        <button
-        onClick={() => addMovieRating(this.props.userId, this.props.movieId, this.state.formInput)}>
-          Submit
-        </button>
+        <input type='submit' value='Submit' />
       </form>
     )
   }
