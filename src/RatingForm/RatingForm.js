b412d6b4f68dd121bd17226c9ec2a5db24357c2f
Jake West <jacobkwest93@gmail.com>
2020-08-24 16:39:18 -0600

    Add state to rating form & updateForm method on change #17

--- src/RatingForm/RatingForm.js
+++ src/RatingForm/RatingForm.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './RatingForm.css';
+import { addMovieRating } from '../NetworkRequests/APIRequests'
 
-const RatingForm = () => {
-  return (
-    <form>
-      <h4>Add Review</h4>
-      <select>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
-      </select>
-      <button onClick={console.log('Added Review!')}>Submit</button>
-    </form>
-  )
+class RatingForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formInput: 0,
+    }
+    this.updateForm = this.updateForm.bind(this)
+  }
+
+  updateForm(event) {
+    const rating = event.target.value;
+    this.setState({ formInput: rating })
+  }
+
+  render() {
+    return (
+      <form>
+        <h4>Add Review</h4>
+        <select value={this.state.formInput} onChange={this.updateForm}>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(number => <option value={number}>{number}</option>)}
+        </select>
+        <button
+        onClick={() => addMovieRating(this.props.userId, this.props.movieId, this.state.formInput)}>
+          Submit
+        </button>
+      </form>
+    )
+  }
 }
 
 export default RatingForm
