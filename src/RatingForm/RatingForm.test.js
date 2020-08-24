27901470c8196f5893b8645b3546222d7e7498fe
Jake West <jacobkwest93@gmail.com>
2020-08-24 17:55:05 -0600

    Add boilerplate code for Rating Form tests #17

---
+++ src/RatingForm/RatingForm.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import RatingForm from './RatingForm'
+import { screen, fireEvent, render } from '@testing-library/react'
+import '@testing-library/jest-dom';
+
+describe('RatingForm', () => {
+  it('', () => {
+
+  })
+})
