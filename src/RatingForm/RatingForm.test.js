70b62d13ee229b0f55794f15ccc57097201d4183
Jake West <jacobkwest93@gmail.com>
2020-08-25 08:18:44 -0600

    Add unit test for Rating Form #25

--- src/RatingForm/RatingForm.test.js
+++ src/RatingForm/RatingForm.test.js
@@ -2,9 +2,15 @@ import React from 'react'
 import RatingForm from './RatingForm'
 import { screen, fireEvent, render } from '@testing-library/react'
 import '@testing-library/jest-dom';
+import { addMovieRating } from '../NetworkRequests/APIRequests'
+jest.mock('../NetworkRequests/APIRequests')
 
 describe('RatingForm', () => {
-  it('', () => {
+  it('Should have the correct content when rendered', () => {
+    render(<RatingForm />)
+    screen.debug()
+    const heading = screen.getByRole('heading', {name: 'Add Review'})
 
+    expect(heading).toBeInTheDocument();
   })
 })
