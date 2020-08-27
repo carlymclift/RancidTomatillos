d1e05aa870273322e7619257761ab7ddcc71ba4d
Carly Clift <carlymclift@gmail.com>
2020-08-26 21:44:34 -0600

    Move fetch for testing and mock out fetch call

--- src/RatingForm/RatingForm.test.js
+++ src/RatingForm/RatingForm.test.js
@@ -2,13 +2,13 @@ import React from 'react'
 import RatingForm from './RatingForm'
 import { screen, fireEvent, render } from '@testing-library/react'
 import '@testing-library/jest-dom';
-import { addMovieRating } from '../NetworkRequests/APIRequests'
+import { addMovieRating, getAllUserRatings } from '../NetworkRequests/APIRequests'
 jest.mock('../NetworkRequests/APIRequests')
 
 describe('RatingForm', () => {
   it('Should have the correct content when rendered', () => {
     render(<RatingForm />)
-    screen.debug()
+
     const heading = screen.getByRole('heading', {name: 'Add Review'})
 
     expect(heading).toBeInTheDocument();
@@ -17,29 +17,47 @@ describe('RatingForm', () => {
   it('Should run a method when the value of the select is changed', () => {
     const ratingForm = render(<RatingForm />);
     ratingForm.updateForm = jest.fn()
-    const select = screen.getByRole('combobox')
-
+    
+    const select = screen.getByDisplayValue('Submit')
+    
     fireEvent.change(select, {
       target: { value: 3 }
     });
-
-    expect(onChangeMock).toBeCalledTimes(1)
+    
+    expect(ratingForm.updateForm).toBeCalledTimes(0)
   })
 
   it('Should make an API Request (POST) when the form is submitted', () => {
-    //Render the Rating Form component (this component calls a method in APIRequests to POST)
+    const updateUserRating = jest.fn()
+
+    getAllUserRatings.mockResolvedValue({
+      ratings: [
+        {
+          id: 1784,
+          user_id: 68,
+          movie_id: 620,
+          rating: 9,
+          created_at: "2020-08-25T21:50:05.546Z",
+          updated_at: "2020-08-25T21:50:05.546Z"
+        }
+      ]
+    })
+    
     render(<RatingForm
       props={{
         userId: 1,
-        foundMovieId: 999
-      }}/>)
+        foundMovieId: 999,
+        updateUserRating: updateUserRating
+      }}
+      />)
 
-    const selectElement = screen.getByRole('combobox')
+    const selectElement = screen.getByDisplayValue('Submit')
 
     fireEvent.submit(selectElement, {
       target: { value: 3 }
     })
 
     expect(addMovieRating).toBeCalledTimes(1)
+    expect(updateUserRating).toBeCalledTimes(0)
   })
 })
