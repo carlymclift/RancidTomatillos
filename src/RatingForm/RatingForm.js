36883b54879225e78815ae1c67a55944342c9c85
Carly Clift <carlymclift@gmail.com>
2020-08-30 00:37:37 -0600

    Reformat for better readability

--- src/RatingForm/RatingForm.js
+++ src/RatingForm/RatingForm.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 import './RatingForm.css';
+import shortid from 'shortid'
 import { Link } from 'react-router-dom'
-import { addMovieRating, getAllUserRatings, removeRating, addMovieComment } from '../NetworkRequests/APIRequests'
+import fakeUserComments from '../fakeComments'
+import CommentCard from '../CommentCard/CommentCard'
+import { addMovieRating, getAllUserRatings, removeRating, addMovieComment, getMovieComments } from '../NetworkRequests/APIRequests'
 
 class RatingForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       formInput: '',
-      comment: ''
+      comment: '',
+      postedComment: '',
+      allMovieComments: [],
+      error: ''
     }
     this.updateForm = this.updateForm.bind(this)
     this.deleteRating = this.deleteRating.bind(this)
     this.updateCommentState = this.updateCommentState.bind(this)
-    // this.updateMovieRating = this.updateMovieRating.bind(this)
+  }
+
+  async componentDidMount() {
+    try {
+      let commentsFromUsers = await getMovieComments(this.props.foundMovieId)
+      let comment = commentsFromUsers.comments.find(comment => comment.author === this.props.userName)
+      console.log(comment)
+      if (comment !== undefined) {
+        this.setState({ postedComment: comment.comment })
+      }
+    } catch (error) {
+      this.setState({error: error})
+    }
+    
   }
 
   updateForm(event) {
@@ -26,16 +45,17 @@ class RatingForm extends Component {
     const comment = event.target.value
     this.setState({ comment: comment })
     console.log(this.state.comment)
-    console.log(this.state.comment.length)
+    // console.log(this.state.comment.length)
   }
 
   submitRating = async (event) => {
     event.preventDefault();
-    if (this.state.formInput !== '') {
+    if (this.state.formInput === '') {
       alert('Please select a rating to submit a review.')
     } else if (this.state.comment.length > 1) {
-      addMovieComment(this.props.foundMovieId, this.state.comment, 'Frank H')
-      // this.submitRatingNumber(event)
+      addMovieComment(this.props.foundMovieId, this.state.comment, this.props.userName)
+      this.setState({ postedComment: this.state.comment })
+      this.submitRatingNumber(event)
     } else if (this.state.comment === '') {
       this.submitRatingNumber(event)
     }
@@ -59,7 +79,13 @@ class RatingForm extends Component {
   }
 
   render() {
-    console.log(this.props)
+    const commentsOnMovie = fakeUserComments.filter(comment => {
+      return comment.movieId === this.props.foundMovieId
+    })
+    const commentCards = commentsOnMovie.map(comment => {
+      return (<CommentCard {...comment} key={shortid.generate()} />)
+    })
+
     return (
       <div className="RatingForm-rating-sec">
         {(!this.props.isLoggedIn &&
@@ -69,9 +95,6 @@ class RatingForm extends Component {
             <Link to='/login'>Login</Link>
           </>
         )}
-        
-            {/* <div> */}
-
         {(this.props.isLoggedIn && this.props.userRating.includes(`You rated this movie`) &&
           <>
             <h2>{this.props.userRating}</h2>
@@ -84,16 +107,14 @@ class RatingForm extends Component {
                 </div>
               <input type='submit' value='Edit Your Rating' className="RatingForm-button"/>
             </form>
-            {/* <button onClick={this.deleteRating}>Edit Rating</button> */}
           </>
         )}
-            {/* </div> */}
         {(this.props.isLoggedIn && this.props.userRating === 'You haven\'t rated this movie yet' &&
           <>
             <h2>Review {this.props.movie.title}</h2>
             <div className="RatingForm-submit-sec">
               <form onSubmit={this.submitRating} className='RatingForm-form'>
-                <textarea onChange={this.updateCommentState} value={this.state.comment} className="RatingForm-comment-form" rows="30" cols="20" wrap="hard" maxLength="400" placeholder="What did you think of this movie? (optional)"></textarea>
+                <textarea onChange={this.updateCommentState} value={this.state.comment} className="RatingForm-comment-form" rows="30" cols="20" wrap="hard" maxLength="500" placeholder="What did you think of this movie? (optional)"></textarea>
                 <div>
                   <select value={this.state.formInput} onChange={this.updateForm} className='RatingForm-dropdown'>
                     {['', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(number => <option key={number} value={number}>{number}</option>)}
@@ -105,44 +126,19 @@ class RatingForm extends Component {
             </div>
           </>
         )}
-
         <div className="RatingForm-comment-sec">
-          <div className="RatingForm-comment-cards">
-            <p>Sandra H</p>
-            <p>This is a paragraph to test the comment section of the page. This is where someone would leave there rating for this movie.</p>
-          </div>
-          <div className="RatingForm-comment-cards">
-            <p>Jessie T</p>
-            <p>This is a paragraph to test the comment section of the page. This is where someone would leave there rating for this movie.</p>
-          </div>
-          <div className="RatingForm-comment-cards">
-            <p>This is a paragraph to test the comment section of the page. This is where someone would leave there rating for this movie.</p>
-          </div>
-          <div className="RatingForm-comment-cards">
-            <p>This is a paragraph to test the comment section of the page. This is where someone would leave there rating for this movie.</p>
-          </div>
-          <div className="RatingForm-comment-cards">
-            <p>This is a paragraph to test the comment section of the page. This is where someone would leave there rating for this movie.</p>
-          </div>
-          <div className="RatingForm-comment-cards">
-            <p>This is a paragraph to test the comment section of the page. This is where someone would leave there rating for this movie.</p>
-          </div>
-          <div className="RatingForm-comment-cards">
-            <p>This is a paragraph to test the comment section of the page. This is where someone would leave there rating for this movie.</p>
-          </div>
-          <div className="RatingForm-comment-cards">
-            <p>This is a paragraph to test the comment section of the page. This is where someone would leave there rating for this movie.</p>
-          </div>
-          <div className="RatingForm-comment-cards">
-            <p>This is a paragraph to test the comment section of the page. This is where someone would leave there rating for this movie.</p>
-          </div>
+          {(this.state.postedComment !== '' &&
+            <>
+              <div className="RatingForm-user-comment">
+                <p>You Posted:</p>
+                <p>{this.state.postedComment}</p>
+                <button onClick={this.deleteComment}>Delete Comment</button>
+               </div>
+            </>
+          )}
+          {commentCards}
         </div>
-
-
       </div>
-
-      // </div>
-
     )
   }
 }
