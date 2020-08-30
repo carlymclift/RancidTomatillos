450deb68767a941e8664b5b03d3a45d38aa40cc9
Carly Clift <carlymclift@gmail.com>
2020-08-30 00:37:16 -0600

    Reformat for better readability

--- src/MovieDetails/MoviePage.js
+++ src/MovieDetails/MoviePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import './MoviePage.css';
-import { removeRating, getSingleMovieDetails } from '../NetworkRequests/APIRequests'
+import { getSingleMovieDetails } from '../NetworkRequests/APIRequests'
 import RatingForm from '../RatingForm/RatingForm'
 
 class MoviePage extends Component {
@@ -14,7 +14,6 @@ class MoviePage extends Component {
     };
     this.formatBudgetAndRevenue = this.formatBudgetAndRevenue.bind(this)
     this.findUserRatingsForFoundMovie = this.findUserRatingsForFoundMovie.bind(this);
-    // this.deleteRating = this.deleteRating.bind(this)
     this.formatAvRating = this.formatAvRating.bind(this)
   }
 
@@ -22,20 +21,16 @@ class MoviePage extends Component {
     try {
       const movie = await getSingleMovieDetails(this.props.foundMovieId)
       const movieInfo = movie.movie
-      // console.log(movieInfo)
       this.setState({
         movie: movieInfo
       })
     } catch (error) {
       this.setState({error: error})
     }
-    // console.log(this.state.movie)
-    // console.log(this.state)
     this.findUserRatingsForFoundMovie()
   }
 
   formatBudgetAndRevenue(x) {
-    // console.log(x)
     if (x === 0 || x === undefined) {
       return 'Not Available'
     } 
@@ -49,7 +44,6 @@ class MoviePage extends Component {
     const userRating = this.props.userRatings.ratings.find(rating => {
       return this.props.foundMovieId === rating.movie_id
     })
-
     if (userRating === undefined && this.props.isLoggedIn) {
       this.setState({userRating: 'You haven\'t rated this movie yet'})
     } else if (!this.props.isLoggedIn) {
@@ -62,20 +56,13 @@ class MoviePage extends Component {
 
   formatAvRating() {
       const shortedRating = this.state.movie.average_rating.toFixed()
-      // console.log(shortedRating)
       this.setState({averageRatingDecimal: shortedRating})
   }
 
-  // deleteRating() {
-  //   const ratingId = this.state.userRatingObj.id
-  //   removeRating(this.props.userId, ratingId)
-  // }
-
   render() {
-    // console.log(this.state.averageRatingDecimal)
     const budget = this.formatBudgetAndRevenue(this.state.movie.budget)
     const revenue = this.formatBudgetAndRevenue(this.state.movie.revenue)
-    // console.log(this.props)
+
     return (
       <div className="Movie-Page" style={{
         backgroundImage: `url(${this.state.movie.backdrop_path})`}}>
@@ -93,18 +80,18 @@ class MoviePage extends Component {
             <p>Release Date: {this.state.movie.release_date}</p>
             <p>Budget: {budget}</p>
             <p>Runtime: {this.state.movie.runtime} minutes</p>
-            <p>Revenue: {revenue}</p>
-            {/* <div> */}
-            {/* {(this.props.isLoggedIn && this.state.userRating === `You rated this movie: ${this.state.userRatingObj.rating}`) &&
-              <button onClick={this.deleteRating}>Delete</button>
-            } */}
-            {/* </div> */}
-            
+            <p>Revenue: {revenue}</p>  
           </div>
         </div>
-        {/* {(this.props.isLoggedIn && this.state.userRating === 'You haven\'t rated this movie yet') && */}
-            <RatingForm foundMovieId={this.props.foundMovieId} isLoggedIn={this.props.isLoggedIn} updateUserRating={this.props.updateUserRating} userId={this.props.userId} movie={this.state.movie} userRating={this.state.userRating} userRatingObj={this.state.userRatingObj} userName={this.props.userName}/>
-        {/* // } */}
+        <RatingForm 
+          foundMovieId={this.props.foundMovieId} 
+          isLoggedIn={this.props.isLoggedIn} 
+          updateUserRating={this.props.updateUserRating} 
+          userId={this.props.userId} 
+          movie={this.state.movie} 
+          userRating={this.state.userRating} 
+          userRatingObj={this.state.userRatingObj} 
+          userName={this.props.userName}/>
       </div>
     )
   }
