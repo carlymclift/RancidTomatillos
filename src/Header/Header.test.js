f1a1ad41020c206bfc33546110d4ab96ecf381e9
Jake West <jacobkwest93@gmail.com>
2020-09-07 17:32:29 -0600

    Add test for Header content when user is logged in #9

--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -4,9 +4,11 @@ import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
 describe('Header', () => {
-    it('should render the correct content on load', () => {
+    it('Should render the correct content when a user is not logged in', () => {
         render(<MemoryRouter> 
-            <Header />
+            <Header 
+                isLoggedIn={false}
+            />
         </MemoryRouter>)
 
         const websiteHeader = screen.getByRole('heading', { name: 'Rancid Tomatillos' })
@@ -17,6 +19,33 @@ describe('Header', () => {
         expect(websiteHeader).toBeInTheDocument();
         expect(homeButton).toBeInTheDocument();
         expect(loginButton).toBeInTheDocument();
+        expect(welcomeMessage).toBeInTheDocument();
+    })
+
+    it('Should render the correct content when a user is logged in', () => {
+        render(<MemoryRouter>
+            <Header 
+                isLoggedIn={true}
+                user={
+                    {
+                        name: 'Cher',
+                        id: 1,
+                        email: 'cher@turing.io'
+                    }
+                }
+            />
+        </MemoryRouter>)
+
+        const websiteHeader = screen.getByRole('heading', { name: 'Rancid Tomatillos' })
+        const homeButton = screen.getByRole('link', { name: 'Home' })
+        const logoutButton = screen.getByRole('link', { name: 'Logout' })
+        const favoritesButton = screen.getByRole('link', { name: 'Favorites' })
+        const welcomeMessage = screen.getByRole('heading', { name: 'Welcome, Cher!' })
+
+        expect(websiteHeader).toBeInTheDocument();
+        expect(homeButton).toBeInTheDocument();
+        expect(logoutButton).toBeInTheDocument();
+        expect(favoritesButton).toBeInTheDocument();
         expect(welcomeMessage).toBeInTheDocument();
     })
 })
