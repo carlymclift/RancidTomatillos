880d58c7a9a28e0a84d99d126e815c282d7fe614
Jake West <jacobkwest93@gmail.com>
2020-09-07 16:59:00 -0600

    Add unit test for Header content #9

--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -1,7 +1,22 @@
+import Header from './Header'
 import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('Header', () => {
-    it('', () => {
-        
+    it('should render the correct content on load', () => {
+        render(<MemoryRouter> 
+            <Header />
+        </MemoryRouter>)
+
+        const websiteHeader = screen.getByRole('heading', { name: 'Rancid Tomatillos' })
+        const homeButton = screen.getByRole('link', { name: 'Home' })
+        const loginButton = screen.getByRole('link', { name: 'Login' })
+        const welcomeMessage = screen.getByRole('heading', { name: 'Welcome!' })
+
+        expect(websiteHeader).toBeInTheDocument();
+        expect(homeButton).toBeInTheDocument();
+        expect(loginButton).toBeInTheDocument();
+        expect(welcomeMessage).toBeInTheDocument();
     })
 })
